bf44bcd9a81d31f73308b36842b11d1a023eca2a
Jeiel Alves <jeielalves25@gmail.com>
2024-06-28 15:53:17 -0300

    update reservation controller using reservation_router outside the ReservationController

--- src/controller/reservation_controller.py
+++ src/controller/reservation_controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+
 from src.dto.reservationDto import ReservationDto
 from src.service.reservation_service import ReservationService
 from src.repository.reservation_repository import ReservationRepository
@@ -7,32 +8,37 @@ from src.database.mysql_connection import get_database_connection
 def get_reservation_service():
     return ReservationService(ReservationRepository(get_database_connection()))
 
+reservation_router = APIRouter(prefix="/parking")
+
+@reservation_router.get("/")
+def find_all(reservation_service: ReservationService = Depends(get_reservation_service)):
+    return reservation_service.find_all()
+
+
+@reservation_router.post("/")
+def create(reservationDto: ReservationDto, reservation_service: ReservationService = Depends(get_reservation_service)):
+    return reservation_service.create(reservationDto)
+
+
+@reservation_router.get("/{reservation_id}")
+def find_by_id(reservation_id: int, reservation_service: ReservationService = Depends(get_reservation_service)):
+    return reservation_service.find_by_id(reservation_id)
+
+
+@reservation_router.delete("/{reservation_id}")
+def delete_by_id(reservation_id: int, reservation_service: ReservationService = Depends(get_reservation_service)):
+    reservation_service.delete(reservation_id)
+    return f"Deleted {reservation_id} successfully!"
+
+
+@reservation_router.put("/{reservation_id}")
+def update_by_id(reservation_id: int, reservationDto: ReservationDto, reservation_service: ReservationService = Depends(get_reservation_service)):
+    return reservation_service.update(reservation_id, reservationDto)
+
 class ReservationController:
-    reservation_router = APIRouter(prefix="/parking")
 
     def __init__(self) -> None:
-        pass
+        self.router = reservation_router
 
     def get_router(self) -> APIRouter:
-        @self.reservation_router.get("/")
-        def find_all(reservation_service: ReservationService = Depends(get_reservation_service)):
-            return reservation_service.find_all()
-
-        @self.reservation_router.post("/")
-        def create(reservationDto: ReservationDto, reservation_service: ReservationService = Depends(get_reservation_service)):
-            return reservation_service.create(reservationDto)
-
-        @self.reservation_router.get("/{reservation_id}")
-        def find_by_id(reservation_id: int, reservation_service: ReservationService = Depends(get_reservation_service)):
-            return reservation_service.find_by_id(reservation_id)
-
-        @self.reservation_router.delete("/{reservation_id}")
-        def delete_by_id(reservation_id: int, reservation_service: ReservationService = Depends(get_reservation_service)):
-            reservation_service.delete(reservation_id)
-            return f"Deleted {reservation_id} successfully!"
-
-        @self.reservation_router.put("/{reservation_id}")
-        def update_by_id(reservation_id: int, reservationDto: ReservationDto, reservation_service: ReservationService = Depends(get_reservation_service)):
-            return reservation_service.update(reservation_id, reservationDto)
-
-        return self.reservation_router
+        return self.router
