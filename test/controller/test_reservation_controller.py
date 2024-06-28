63764347501b382620d31c8e791d01fea5b0a727
Jeiel Alves <jeielalves25@gmail.com>
2024-06-28 16:08:28 -0300

    add mock dependency injection in test_reservation_controller

--- test/controller/test_reservation_controller.py
+++ test/controller/test_reservation_controller.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from unittest import TestCase
-# from unittest.mock import MagicMock
+from unittest.mock import MagicMock
 # import mysql.connector
 
 
@@ -21,17 +21,18 @@ sys.path.append(correctPath)
 
 from fastapi.testclient import TestClient
 from src.main import app
+from src.controller.reservation_controller import get_reservation_service
 
-# mock_session = MagicMock()
+mock_session = MagicMock()
 
-# def override_get_db():
-#     try:
-#         yield mock_session
-#     finally:
-#         pass
+def override_get_db():
+    try:
+        yield mock_session
+    finally:
+        pass
 
 
-# app.dependency_overrides[get_database_connection] = override_get_db
+app.dependency_overrides[get_reservation_service] = override_get_db
 
 client = TestClient(app)
 
@@ -41,3 +42,8 @@ class TestReservationController(TestCase):
         response = client.get("/", headers={"X-Token": "coneofsilence"})
         assert response.status_code == 200
         assert response.json() == "Server is running"
+
+    def test_find_all(self):
+        response = client.get("/parking", headers={"X-Token": "coneofsilence"})
+        assert response.status_code == 200
+
